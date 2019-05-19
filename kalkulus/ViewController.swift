0406f4ac773b90cde7367458bef46e9491ead96d
oppmacsom <oppmacsom@gmail.com>
2019-05-20 01:16:15 +0200

    Initial Commit

---
+++ kalkulus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kalkulus
+//
+//  Created by oppmacsom on 20/05/2019.
+//  Copyright © 2019 oppmacsom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
